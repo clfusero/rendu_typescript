12446fa8de6b4b485cdfcf0f554eb3086229bb29
clement.fusero <clement.fusero@gmail.com>
2025-09-13 10:28:51 +0200

    import de la classe car

---
+++ main.ts
@@ -0,0 +1,9 @@
+// Import de la classe Car
+import { Car } from "./Car";
+
+const myCar = new Car("Clio", "Renault", "Rouge", 2020);
+
+myCar.start();
+myCar.accelerate(30);
+myCar.accelerate(20);
+myCar.stop();
